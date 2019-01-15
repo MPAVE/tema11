1490862c4b991fb015b51119f60cbe27b028e07f
MPAVE <45466369+MPAVE@users.noreply.github.com>
2019-01-15 17:54:35 +0200

    Add files via upload

---
+++ person class/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonClass
+{
+    class Person
+    { public string Name { get; set; }
+        public int? Age { get; set; }
+        
+    
+
+        public Person(string name, int? age = null)
+        {
+            this.Name = name;
+            this.Age = age;
+
+        }
+        public override string ToString()
+        {
+            var x = new StringBuilder();
+            x.Append(string.Format(this.Name, this.Age));
+            return x.ToString();
+        }
+    }
+}
---
+++ person class/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Mirela = new Person("Mirela00", 25);
+            var Ioana = new Person("Ioana01");
+            Console.WriteLine(Mirela);
+            Console.ReadLine();
+        }
+    }
+}
