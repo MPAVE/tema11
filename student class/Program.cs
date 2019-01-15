a17d637ecd912d4a751c975aac664b5937af79de
MPAVE <45466369+MPAVE@users.noreply.github.com>
2019-01-15 17:55:30 +0200

    Add files via upload

---
+++ student class/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Popa", "Ion", 1234567891, "Str.AntonPann,Bl.4,Sc.C,Ap.4","+07475777489","PopaIon@gmail.com",2,Speciality.InformaticaEcnomica,Universities.AlexandruIoanCuza,Faculties.Feaa);
+            Student student2 = new Student("Gheorghe", "Paul", 1234567890, "Str.AntonPann,Bl.4,Sc.C,Ap.4", "+07475777489", "PaulGh@gmail.com", 2, Speciality.Management, Universities.AlexandruIoanCuza, Faculties.AutomaticasiCalculatoare);
+
+            Student[] students = new Student[] { student1, student2 };
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+                
+
+            }
+            
+
+            //Equals()
+            Console.WriteLine(student1.Equals(student2));
+
+            //GetHashCode()
+            Console.WriteLine(student1.GetHashCode());
+            Console.WriteLine(student2.GetHashCode());
+            //== !=
+            Console.WriteLine(student1==student2);
+            Console.WriteLine(student1!=student2);
+
+            //clone()
+            Console.WriteLine("student1 este clonat");
+            var student11 = student1.Clone() ;
+            //compareto()
+            Console.WriteLine();
+            Console.WriteLine(student1.CompareTo(student11));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ student class/Student.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student_class
+{
+    class Student : ICloneable, IComparable
+    {
+        public Student(string firstname, string lastname, int ssn, string address, string mobile, string email, int course, Speciality specializari, Universities universitati, Faculties facultati)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.SSN = ssn;
+            this.Address = address;
+            this.Mobile = mobile;
+            this.Email = email;
+            this.Course = course;
+            this.Specializare = specializari;
+            this.Universitati = universitati;
+            this.Facultati = facultati;
+
+        }
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int SSN { get; set; }
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public int Course { get; set; }
+        public Speciality Specializare { get; set; }
+        public Universities Universitati { get; set; }
+        public Faculties Facultati { get; set; }
+
+        //Equals()
+        public override bool Equals(object obj)
+        {
+            var objStudent = obj as Student;
+
+            return this.FirstName == objStudent.FirstName;
+        }
+        //GetHashCode()
+        public override int GetHashCode()
+        {
+            int h = 6;
+            h = (h * 6) + this.SSN.GetHashCode();
+            return h;
+        }
+        //tostring()
+        public override string ToString()
+        {
+            var x = new StringBuilder();
+            x.Append(string.Format(this.FirstName, this.LastName));
+                
+            
+            return x.ToString();
+        }
+        //== !=
+        public static bool operator ==(Student firstStudent, Student secondStudent)
+        {
+            return firstStudent.Equals(secondStudent);
+        }
+        public static bool operator !=(Student firstStudent, Student secondStudent)
+        {
+            return !(firstStudent == secondStudent);
+        }
+        //Clone()
+        public object Clone()
+        {
+            Student curentStudent = this;
+            Student newStudent = new Student(curentStudent.FirstName, curentStudent.LastName, curentStudent.SSN, curentStudent.Address, curentStudent.Mobile, curentStudent.Email, curentStudent.Course, curentStudent.Specializare, curentStudent.Universitati, curentStudent.Facultati);
+            return newStudent;
+         }
+        //CompareTo()
+        public int CompareTo(object obj)
+        {
+            Student objStudent = obj as Student;
+            int c = this.FirstName.CompareTo(objStudent.FirstName);
+            if(c==0)
+            {
+                return this.SSN.CompareTo(objStudent.SSN);
+            }
+            return c;
+        }
+
+
+    }
+    //Enum
+        public enum Speciality
+        {InformaticaEcnomica,
+            Finante,
+            Marketing,
+            Management,
+            Contabilitate
+        }
+
+        public enum Universities
+        {AlexandruIoanCuza,
+            GheorgheAsachi
+
+        }
+
+        public enum Faculties
+        {Feaa,
+            AutomaticasiCalculatoare
+
+        }
+
+}
