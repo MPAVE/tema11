aae959a3dd1412b5d1ccf8190491b9230a0a70e6
MPAVE <45466369+MPAVE@users.noreply.github.com>
2019-01-15 17:56:05 +0200

    Add files via upload

---
+++ 64bitarray/BitArray64.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _64bit_array
+{
+    class BitArray64
+    {
+        public const byte Bits = 64;
+        public ulong Number { get; set; }
+        public BitArray64(ulong number)
+        {
+            this.Number = number;
+        }
+        public override bool Equals(object obj)
+        {
+            var objBit = obj as BitArray64;
+            return this.Number ==objBit.Number;
+        }
+        
+        public static bool operator ==(BitArray64 firstBit, BitArray64 secondBit)
+        {
+            return firstBit.Equals(secondBit);
+        }
+        public static bool operator!=(BitArray64 firstBit,BitArray64 secondBit)
+        {
+            return !(firstBit == secondBit);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode() ^ this.Number.GetHashCode();
+        }
+
+
+
+
+    }
+}
---
+++ 64bitarray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _64bit_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitArray64 arr1 = new BitArray64(4U);
+            BitArray64 arr2 = new BitArray64(250U);
+
+            Console.WriteLine(arr1);
+            Console.WriteLine(); ;
+            Console.WriteLine(arr2);
+
+            Console.WriteLine(arr1==arr2);
+            Console.WriteLine();
+            Console.WriteLine(arr1.Equals(arr2));
+            Console.WriteLine();
+            Console.WriteLine(arr1!=arr2);
+
+            Console.WriteLine(arr1.GetHashCode());
+            Console.ReadLine();
+        }
+    }
+}
