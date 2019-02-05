da337663caebc699e6996007b65e088a2b7a5bbe
MPAVE <45466369+MPAVE@users.noreply.github.com>
2019-02-05 12:37:00 +0200

    Add files via upload

---
+++ tema14/InsertPublisherApp/Print.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertPublisherApp
+{
+    class Print
+    {
+        public static void PrintPublishers()
+        {
+            SqlConnection connection = new SqlConnection { ConnectionString = "Data Source=DESKTOP-90EKCKC\\Mirela;Initial Catalog=homeworksql3;Integrated Security=True" };
+            connection.Open();
+
+            string printPublishers = "select PublisherID, Name from Publisher";
+            SqlCommand showPublishers = new SqlCommand(printPublishers);
+            showPublishers.Connection = connection;
+
+            SqlDataReader reader = showPublishers.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine($"\t {reader["PublisherID"]} - {reader["Name"]}");
+            }
+            reader.Close();
+            connection.Close();
+        }
+        public static void PrintBooks()
+        {
+            SqlConnection connection = new SqlConnection { ConnectionString = "Data Source=DESKTOP-90EKCKC\\Mirela;Initial Catalog=homeworksql3;Integrated Security=True" };
+
+            SqlCommand printBooks = new SqlCommand("select BookID, Title, Year, Price, Name from Book b inner join Publisher p on b.PublisherID=p.PublisherID;");
+            printBooks.Connection = connection;
+            SqlDataReader reader = printBooks.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine($"\t id: {reader[0]}    title: '{reader[1]}'    year: {reader[2]},    price: {reader[3]} lei,    publisher '{reader[4]}'");
+            }
+            reader.Close();
+
+            connection.Close();
+        }
+    }
+}
---
+++ tema14/InsertPublisherApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertPublisherApp
+{
+    class Program
+    {
+        //     Create a console app named InsertPublisherApp to connect to local database.
+        //With this console read the name of publisher and insert a new publisher to database.
+        //Use SQL parameters for that.
+        //Print the inserted id(Execute scalar with select identity)
+        public static void InsertPublisher(string publisherName)
+        {
+            SqlConnection connection = new SqlConnection { ConnectionString = "Data Source=DESKTOP-90EKCKC\\Mirela;Initial Catalog=homeworksql3;Integrated Security=True" };
+            connection.Open();
+
+            SqlParameter publisherNameParameter = new SqlParameter();
+            publisherNameParameter.Value = publisherName;
+            publisherNameParameter.ParameterName = "publisherNameParameter";
+
+            SqlCommand insertPublisher = new SqlCommand();
+            insertPublisher.Connection = connection;
+            insertPublisher.CommandText = "INSERT INTO Publisher(Name) VALUES(@publisherNameParameter);"
+                                         + "SELECT CAST(scope_identity() AS int)";
+
+            insertPublisher.Parameters.Add(publisherNameParameter);
+
+            int inserted = (int)insertPublisher.ExecuteScalar();
+            Console.WriteLine($"\n\t Inserted PublisherID: {inserted}");
+
+            connection.Close();
+        }
+    
+
+        static void Main(string[] args)
+        {
+                
+                Print.PrintPublishers();
+
+                Console.WriteLine("\n\t Insert a new publisher to database");
+                Console.WriteLine("\n\t Please enter the name of the Publisher to insert: ");
+                Program.InsertPublisher(Console.ReadLine());
+
+                Print.PrintPublishers();
+
+                Console.ReadLine();
+            }
+    }
+}
